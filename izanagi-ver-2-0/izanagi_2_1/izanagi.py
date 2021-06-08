c38fadc819230f58c4e411986f3dfac3e7098124
Katsumi Kougen <lesanonyme1001@gmail.com>
2021-06-08 10:27:54 +0700

    Fixed the code

--- izanagi-ver-2-0/izanagi_2_1/izanagi.py
+++ izanagi-ver-2-0/izanagi_2_1/izanagi.py
@@ -3,7 +3,8 @@ import random as r
 
 cursor=[1,1]
 u_color=("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f")
-px=[0]*800
+color=u_color[0]
+px=[color]*800
 isdarkpalette=True
 
 def border():
